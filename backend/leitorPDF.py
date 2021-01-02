dbbb3688af21f9955f8c845b229ad9ea75213add
Daniel Stock <danielcstock12@gmail.com>
2021-01-02 19:56:08 -0300

    Commit inicial

---
+++ backend/leitorPDF.py
@@ -0,0 +1,5 @@
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+from urllib.request import urlopen
